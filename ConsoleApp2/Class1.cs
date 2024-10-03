4669941c002e05b4c1303dc224aed08b9ea49b11
Максим Бойко <mboyko2005@gmail.com>
2024-10-03 14:32:20 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Class1
+{
+	public class MyFunctions
+	{
+		// 1. Функция сложения двух чисел
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		// 2. Функция вычитания двух чисел
+		public int Subtract(int a, int b)
+		{
+			return a - b;
+		}
+
+		// 3. Функция умножения двух чисел
+		public int Multiply(int a, int b)
+		{
+			return a * b;
+		}
+
+		// 4. Функция деления двух чисел
+		public double Divide(int a, int b)
+		{
+			if (b == 0)
+				throw new DivideByZeroException("Деление на ноль невозможно.");
+			return (double)a / b;
+		}
+
+		// 5. Функция для вывода сообщения
+		public void PrintMessage(string message)
+		{
+			Console.WriteLine($"Сообщение из DLL: {message}");
+		}
+	}
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Class1; // Правильное пространство имен вашей DLL
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Создаём экземпляр класса из DLL
+			MyFunctions functions = new MyFunctions();
+
+			// Используем функцию сложения
+			int sum = functions.Add(10, 5);
+			Console.WriteLine($"10 + 5 = {sum}");
+
+			// Используем функцию вычитания
+			int difference = functions.Subtract(10, 5);
+			Console.WriteLine($"10 - 5 = {difference}");
+
+			// Используем функцию умножения
+			int product = functions.Multiply(10, 5);
+			Console.WriteLine($"10 * 5 = {product}");
+
+			// Используем функцию деления
+			try
+			{
+				double quotient = functions.Divide(10, 5);
+				Console.WriteLine($"10 / 5 = {quotient}");
+			}
+			catch (DivideByZeroException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+			// Используем функцию вывода сообщения
+			functions.PrintMessage("Привет из DLL!");
+
+			Console.WriteLine("Нажмите любую клавишу для выхода...");
+			Console.ReadKey();
+		}
+	}
+}
